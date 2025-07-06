809e3daa928d0f186926a8b534908155d1f5f402
yeolmu96 <jeong7653@naver.com>
2025-07-06 17:00:08 +0900

    [feat] 섹션5_컬렉션 조회 최적화ing
    19강_주문조회 V3_엔티티를 DTO로 변환_패치 조인 최적화(완료)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,22 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+<<컬렉션 패치 조인>>
+1:n 관계에서 fetch join -> 페이징 불가능/1개만 사용 가능
+ */
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +24,70 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
+    //fetch join 사용
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> rodersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(OrderDto::new)
+//                .map(o -> new OrderDto(o))
+                .collect(toList());
+        return collect;
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems; //OrderItem으로 받으면 안 되고 Dto로 맵핑(객체 직접 노출 방지)
+
+        //생성자
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(toList());
+
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+//            orderItems = order.getOrderItems();
+        }
+    }
+
+    @Data
+    static class OrderItemDto{
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        //생성자
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName(); //강제 초기화
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
-
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
-
         for (Order order : all) {
             order.getMember().getName();
             order.getDelivery().getAddress();
@@ -29,7 +96,6 @@ public class OrderApiController {
             List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream().forEach(o -> o.getItem().getName());
         }
-
         return all;
     }
 
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -98,4 +98,25 @@ public class OrderRepository {
                         " join fetch o.delivery"
                 , Order.class).getResultList();
     }
+
+    //Order와 관련된 모든 데이터(Member, Delivery, OrderItems, Item)를 단 1쿼리로 즉시 로딩해서 N+1 문제를 방지
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o " +
+                        //db의 distinct는 모든 속성값이 같아야 중복 제거하지만, jpa에서는 entity가 같으면 중복 제거
+                "join fetch o.member m " +
+                "join fetch o.delivery d " +
+                "join fetch o.orderItems oi " + //1:n
+                "join fetch oi.item i", Order.class)
+        //페이징처리
+//        .setFirstResult(1)
+//        .setMaxResults(100)
+        .getResultList();
+        /*
+        WARN 1672 --- [nio-8080-exec-1] org.hibernate.orm.query
+         : HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory
+         -> 메모리에서 페이징 처리해버림(out of memory 주의!!!!)
+         -> orderitem을 기준으로 페이징 함(order entity 기준이 아니라)
+         */
+    }
 }
